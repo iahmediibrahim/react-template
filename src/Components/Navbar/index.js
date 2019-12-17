6e09df73f0293dc1a3366a8157d7457bda4196da
swethusiast <aitc.dev@gmail.com>
2019-12-17 13:32:30 +0200

    Navbar as styledComponent

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavbarSection, Logo, LogoText, UlList, ListItem, Anchor } from './style.js';
+
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
+    <NavbarSection>
+      <div className="container">
+        <Logo>
+          <LogoText>Ultra Profile</LogoText>
+        </Logo>
+        <UlList>
+          <ListItem>
+            <Anchor to="/">Home</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor href="#">Portfolio</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor href="#">Resume</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor href="#">About</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor to="/contact">Contact</Anchor>
+          </ListItem>
+        </UlList>
+      </div>
+    </NavbarSection>
   );
 };
 
